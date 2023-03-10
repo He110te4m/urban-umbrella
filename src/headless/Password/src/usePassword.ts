f612730dd7762dfad3c88e80a10c9f121e302445
He110te4m <me@he110.site>
2023-03-10 09:28:15 +0800

    feat(password): init usePassword

--- src/headless/Password/src/usePassword.ts
+++ src/headless/Password/src/usePassword.ts
@@ -1,9 +1,16 @@
-import { ref } from 'vue'
-
 interface UsePasswordOption {
-  initValue: string
+  initValue?: string
+  maskChar?: string
 }
 
-export function usePassword({ initValue }: UsePasswordOption) {
-  return ref(initValue)
+export function usePassword({ initValue = '', maskChar = '*' }: UsePasswordOption = {}) {
+  const text = ref(initValue)
+
+  return computed({
+    get: () => text.value.split('').map(() => maskChar).join(''),
+    set: (newText: string) => {
+      // TODO: 功能未完成，set 还需要添加 diff
+      text.value = newText
+    },
+  })
 }
